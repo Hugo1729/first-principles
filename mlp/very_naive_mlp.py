a0fa8e8320b062d6ca56a3e65a5fa950caee1e7d
Hugo1729 <hugobruwer4@gmail.com>
2026-08-03 10:08:47 +0200

    added VNMLP class and experimented with normalization and layer sizes

--- mlp/very_naive_mlp.py
+++ mlp/very_naive_mlp.py
@@ -1,32 +1,33 @@
 import numpy as np
 
-layer_sizes = (5000,5000,5)
+class MLP:
+    def __init__(self, layer_sizes):
+        self.weight_shapes = [(layer_sizes[i+1],layer_sizes[i]) for i in range(len(layer_sizes)-1)]
 
-weight_shapes = [(layer_sizes[i+1],layer_sizes[i]) for i in range(len(layer_sizes)-1)]
+        self.weights = [np.random.standard_normal(s)/np.sqrt(s[1]) for s in self.weight_shapes]
+        self.biases = [np.zeros((layer_sizes[i],1)) for i in range(1,len(layer_sizes))]
 
-weights = [np.random.standard_normal(s)/np.sqrt(s[1]) for s in weight_shapes]
-biases = [np.zeros((layer_sizes[i],1)) for i in range(1,len(layer_sizes))]
+    def predict(self, a):
+        for w, b in zip(self.weights, self.biases):
+            a = self.activation((w @ a) + b)
 
-def predict(a):
-    for w, b in zip(weights, biases):
-        a = activation((w @ a) + b)
+        return a
 
-    return a
+    @staticmethod
+    def activation(x):
+        return 1/(1+np.exp(-x))
 
-# @staticmethod
-def activation(x):
-    return 1/(1+np.exp(-x))
 
-print("weights:")
 
-for w in weights:
-    print(w)
-    print()
+layer_sizes = (10,1000,1000,10)
 
-print("biases:")
+mlp = MLP(layer_sizes)
+print(mlp.predict(np.ones((layer_sizes[0],1))))
 
-for b in biases:
-    print(b)
-    print()
 
-print(predict(np.ones((layer_sizes[0],1))))
+#notice variance drift in this sigmoid neural net, interesting discovery
+#Z0 variance: 0.9666736249917937
+#Z1 variance: 0.3070157337172165
+#Z2 variance: 0.12832707290078227
+#this turns out to be well documented and motivates a different activation
+#that preserves variance or we can use batch norm
