69e1e341bcc45189aa30cb839e3465c0ae37df2c
Hugo1729 <hugobruwer4@gmail.com>
2026-08-08 20:20:19 +0200

    big progress on autograd_mlp

--- mlp/autograd_mlp.py
+++ mlp/autograd_mlp.py
@@ -2,17 +2,88 @@ import numpy as np
 
 class Value:
     def __init__(self, val):
-        self._val = np.float64(val)
-        self._grad = np.float64(0)
-        self._children = []
+        self.val = val
+        self._parents = []
+        self.grad = 0.0
+        self._backward = None
 
-    def __str__(self):
-        return str(self._val)
+    def __repr__(self):
+        return f"Value({self.val})"
 
     def __mul__(self, other):
-        ans = Value(self._val * other._val)
-        ans._grad = self._grad + other._grad
-        ans._children.append(self, other)
+        other = other if isinstance(other, Value) else Value(other)
+        ans = Value(self.val * other.val)
+        ans._parents += [self, other]
+
+        def _backward():
+           self.grad += ans.grad * other.val
+           other.grad += ans.grad * self.val
+
+        ans._backward = _backward
+
+        return ans
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        ans = Value(self.val + other.val)
+        ans._parents += [self, other]
+
+        def _backward():
+            self.grad += ans.grad
+            other.grad += ans.grad
+
+        ans._backward = _backward
+
+        return ans
+
+    def __pow__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        ans = Value(self.val ** other.val)
+        ans._parents += [self, other]
+
+        def _backward():
+            self.grad += ans.grad * other * (self.val ** (other - 1))
+            other.grad += ans.grad * ans.val * np.
+
+        ans._backward = _backward
+
+        return ans
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __neg__(self): # -self
+        return self * -1
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def backward(self):
+        nodes = []
+        visited = set()
+        def top_sort(v):
+            if (v in visited):
+                return
+            
+            visited.add(v)
+            for w in v._parents:
+                top_sort(w)
+
+            nodes.append(v)
+
+        self._grad = 1.0
+        top_sort(self)
+
+        for v in reversed(nodes):
+            v._backward()
+
+
 
 layer_sizes = (2,3,3)
 
